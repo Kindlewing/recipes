9cc4cad189082c2a1c30fe40800f39530f78e5cd
hudson <hfinn@autodist.com>
2023-12-18 19:13:34 -0500

    Working on recipe cards

--- src/components/recipeCard.tsx
+++ src/components/recipeCard.tsx
@@ -4,14 +4,20 @@ import data from "../recipes.json";
 
 const RecipeCard = () => {
 	const [recipes, setRecipes] = useState<Array<Recipe> | null>(null);
+
 	useEffect(() => {
 		const json = JSON.parse(JSON.stringify(data));
 		const recipes: Array<Recipe> = [];
 
 		console.log(json);
 		setRecipes(recipes);
-	});
-	return <article className="card"></article>;
+	}, []);
+
+	return (
+		<article className="card">
+			<p className="title"></p>
+		</article>
+	);
 };
 
 export default RecipeCard;
